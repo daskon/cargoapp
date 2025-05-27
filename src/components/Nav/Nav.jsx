2e9593dffbdf1b8149c61a34ec4add6028ba3429
daskon <aselaforever@gmail.com>
2025-05-27 11:27:54 +0530

    nav bar development

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <div className=' bg-amber-300'>Nav</div>
+    <div className=' bg-amber-300'>
+        <div className='w-6 h-6'><img src='/logocargo.png' /></div>
+    </div>
   )
 }
